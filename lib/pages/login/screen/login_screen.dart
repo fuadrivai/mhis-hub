a6e3370d5afe62384f114fcba606c4ae3f6cdc60
fuadrivai <fuad.strummer@gmail.com>
2026-08-31 15:15:24 +0700

    cleaning

--- lib/pages/login/screen/login_screen.dart
+++ lib/pages/login/screen/login_screen.dart
@@ -38,108 +38,114 @@ class LoginScreenState extends State<LoginScreen> {
             color: AppColors.primary,
           ),
         ),
-        body: BlocListener<LoginBloc, LoginState>(
-          listener: (context, state) {
-            if (!context.mounted) return;
-            if (state.isError) {
-              Common.modalInfo(
-                context,
-                title: "Error",
-                message: state.errorMessage,
-                mode: MODE.error,
-                showAction: state.showAction,
-              );
-            }
+        body: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
           },
-          child: SafeArea(
-            child: Stack(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height,
-                  width: MediaQuery.of(context).size.width,
-                  color: AppColors.primary,
-                ),
-                const CustomHeader(text: 'Log In.'),
-                Positioned(
-                  top: MediaQuery.of(context).size.height * 0.08,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.9,
+          behavior: HitTestBehavior.translucent,
+          child: BlocListener<LoginBloc, LoginState>(
+            listener: (context, state) {
+              if (!context.mounted) return;
+              if (state.isError) {
+                Common.modalInfo(
+                  context,
+                  title: "Error",
+                  message: state.errorMessage,
+                  mode: MODE.error,
+                  showAction: state.showAction,
+                );
+              }
+            },
+            child: SafeArea(
+              child: Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height,
                     width: MediaQuery.of(context).size.width,
-                    decoration: const BoxDecoration(
-                      color: AppColors.whiteshade,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40),
+                    color: AppColors.primary,
+                  ),
+                  const CustomHeader(text: 'Log In.'),
+                  Positioned(
+                    top: MediaQuery.of(context).size.height * 0.08,
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.9,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        color: AppColors.whiteshade,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
                       ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 5),
-                        Image.asset(
-                          'assets/images/logo-hub.png',
-                          scale: 1,
-                          height: 150,
-                        ),
-                        // Image.asset('assets/images/logo-hub.png'),
-                        const SizedBox(
-                          height: 24,
-                        ),
-                        CustomFormField(
-                          headingText: "Email",
-                          hintText: "Email",
-                          obsecureText: false,
-                          suffixIcon: const SizedBox(),
-                          controller: _emailController,
-                          maxLines: 1,
-                          textInputAction: TextInputAction.done,
-                          textInputType: TextInputType.emailAddress,
-                        ),
-                        const SizedBox(height: 16),
-                        CustomFormField(
-                          headingText: "Password",
-                          maxLines: 1,
-                          textInputAction: TextInputAction.done,
-                          textInputType: TextInputType.text,
-                          hintText: "At least 8 Character",
-                          obsecureText: isVisible,
-                          suffixIcon: IconButton(
-                            icon: Icon(iconData),
-                            onPressed: () {
-                              setState(() {
-                                isVisible = !isVisible;
-                                iconData = Icons.visibility_off;
-                              });
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 5),
+                          Image.asset(
+                            'assets/images/logo-hub.png',
+                            scale: 1,
+                            height: 150,
+                          ),
+                          // Image.asset('assets/images/logo-hub.png'),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          CustomFormField(
+                            headingText: "Email",
+                            hintText: "Email",
+                            obsecureText: false,
+                            suffixIcon: const SizedBox(),
+                            controller: _emailController,
+                            maxLines: 1,
+                            textInputAction: TextInputAction.done,
+                            textInputType: TextInputType.emailAddress,
+                          ),
+                          const SizedBox(height: 16),
+                          CustomFormField(
+                            headingText: "Password",
+                            maxLines: 1,
+                            textInputAction: TextInputAction.done,
+                            textInputType: TextInputType.text,
+                            hintText: "At least 8 Character",
+                            obsecureText: isVisible,
+                            suffixIcon: IconButton(
+                              icon: Icon(iconData),
+                              onPressed: () {
+                                setState(() {
+                                  isVisible = !isVisible;
+                                  iconData = Icons.visibility_off;
+                                });
+                              },
+                            ),
+                            controller: _passwordController,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                          ),
+                          BlocBuilder<LoginBloc, LoginState>(
+                            builder: (context, state) {
+                              if (state.isLoading) {
+                                return const Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                              return AuthButton(
+                                onTap: () {
+                                  context.read<LoginBloc>().add(OnLogin({
+                                        "email": _emailController.text,
+                                        "password": _passwordController.text
+                                      }));
+                                },
+                                text: 'Sign In',
+                              );
                             },
                           ),
-                          controller: _passwordController,
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 20),
-                        ),
-                        BlocBuilder<LoginBloc, LoginState>(
-                          builder: (context, state) {
-                            if (state.isLoading) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
-                            return AuthButton(
-                              onTap: () {
-                                context.read<LoginBloc>().add(OnLogin({
-                                      "email": _emailController.text,
-                                      "password": _passwordController.text
-                                    }));
-                              },
-                              text: 'Sign In',
-                            );
-                          },
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
